6debf7ebec6bce194c48eac8ab9872ceb256ca50
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-11-03 17:53:15 +0530

    feat: simplify routing by removing unused components and updating navigation structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
-// src/App.jsx
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router';
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import AddProduct from './pages/AddProduct';
+import AuthLaout from './layout/AuthLaout';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import Products from './pages/Products';
-import EntriesExits from './pages/EntriesExits';
-import History from './pages/History';
-import Layout from './components/Layout';
-import { load } from './utils/storage';
-
-export default function App() {
-  const user = load('pi:user', null);
-
+import SignUp from './pages/Signup';
+import NotFoundPage from './pages/NotFoundPage';
+function App() {
   return (
-    <Routes>
-      {!user ? (
-        <>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </>
-      ) : (
-        <>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/entries-exits" element={<EntriesExits />} />
-            <Route path="/history" element={<History />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </>
-      )}
-    </Routes>
+    <>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <AuthLaout>
+              <Login />
+            </AuthLaout>
+          }
+        />
+
+        <Route
+          path="/signup"
+          element={
+            <AuthLaout>
+              <SignUp />
+            </AuthLaout>
+          }
+        />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/addProduct" element={<AddProduct />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
+
+export default App;
