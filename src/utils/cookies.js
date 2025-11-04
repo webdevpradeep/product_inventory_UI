d383825ad832690b058b6c6e2d2f5f10ecb11d6e
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-11-04 18:58:02 +0530

    feat: implement cookie utility functions for setting, getting, and deleting cookies

---
+++ src/utils/cookies.js
@@ -0,0 +1,27 @@
+const setCookie = (name, vaule, exSeconds = 3600) => {
+  if (typeof window !== 'undefined') {
+    const maxAge = `Max-Age=${exSeconds}`;
+    document.cookie = `${name}=${encodeURIComponent(vaule)};
+        ${maxAge}; path =/; SameSite=Lax;`;
+  }
+};
+
+const getCookie = (name) => {
+  const decodeCookie = deCodeURIComponent(document.cookie);
+  const cookiesArray = decodeCookie.split(';');
+
+  for (let cookie of cookiesArray) {
+    cookie = cookie.trim();
+    if (cookie.startWith((name = '='))) {
+      return cookie.substring((name.length = 1));
+    }
+  }
+  return null;
+};
+
+const deletCookie = (name) => {
+  if (typeof window !== 'undefined') {
+    document.cookie = `${name}=; path=/; Max-Age=-1; SameSite=Lax`;
+  }
+};
+export { setCookie, getCookie, deletCookie };
