d214c6c81cc988ba86309f9b70b4e10d932c9441
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-11-04 20:07:32 +0530

    Add Cookie createed

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -7,12 +7,14 @@ const setCookie = (name, vaule, exSeconds = 3600) => {
 };
 
 const getCookie = (name) => {
-  const decodeCookie = deCodeURIComponent(document.cookie);
+  if (typeof window === 'undefined') return null;
+
+  const decodeCookie = decodeURIComponent(document.cookie);
   const cookiesArray = decodeCookie.split(';');
 
   for (let cookie of cookiesArray) {
     cookie = cookie.trim();
-    if (cookie.startWith((name = '='))) {
+    if (cookie.startsWith(name + '=')) {
       return cookie.substring((name.length = 1));
     }
   }
