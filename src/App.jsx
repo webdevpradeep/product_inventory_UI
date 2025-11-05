c564fa0d1c338d63a455da56944038a2c731e0e7
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-11-05 19:51:17 +0530

    ProductsView, Dashboard, Inventory, User, Entry and Exit pages add in…

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from 'react-router';
-import HomePage from './pages/HomePage';
 import AddProduct from './pages/AddProduct';
 import AuthLaout from './layout/AuthLaout';
 import Login from './pages/Login';
 import SignUp from './pages/Signup';
 import NotFoundPage from './pages/NotFoundPage';
+import Dashboard from './pages/Dashboard';
+import ProductsView from './pages/ProductsView';
+import Inventory from './pages/Inventory';
+import User from './pages/User';
+import Entries from './pages/Entries';
+import Exits from './pages/Exits';
+import MainLayout from './layout/MainLayout';
 function App() {
   return (
     <>
@@ -26,7 +32,66 @@ function App() {
             </AuthLaout>
           }
         />
-        <Route path="/" element={<HomePage />} />
+
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Dashboard />
+            </MainLayout>
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <MainLayout>
+              <Dashboard />
+            </MainLayout>
+          }
+        />
+
+        <Route
+          path="/products"
+          element={
+            <MainLayout>
+              <ProductsView />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/inventory"
+          element={
+            <MainLayout>
+              <Inventory />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <MainLayout>
+              <User />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/entries"
+          element={
+            <MainLayout>
+              <Entries />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/exits"
+          element={
+            <MainLayout>
+              <Exits />
+            </MainLayout>
+          }
+        />
+
         <Route path="/addProduct" element={<AddProduct />} />
 
         <Route path="*" element={<NotFoundPage />} />
